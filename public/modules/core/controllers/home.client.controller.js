bb07fafd569bcb9ae5961a14f3e483797cec3ca7
Kevin Ruiz <kevin.ruiz@coderoad.com>
2015-05-08 18:27:36 -0600

    "Adding conection to RasperryPi"

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,45 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+        $scope.lightFlag = false;
+        $scope.lightOnCount = 0;
+        $scope.lightOffCount = 0;
+
+        var socket = io.connect('http://192.168.0.102:8080');
+
+        socket.on('connect', function() {
+            $('#messages').html('Connected to the server.');
+        });
+
+        socket.on('message', function(message) {
+            $('#status').html(message);
+        });
+
+        socket.on('disconnect', function() {
+            $('#messages').html('<li>Disconnected from the server.</li>');
+            ('#status').html('disconnected');
+        });
+
+        $scope.lightButton = function(){
+
+            var messageFlag = $('#switchButton').text();
+            var message = $('#switchButton').text();
+            console.log(message)
+            socket.send(message);
+//            $('#messages').append('<li>me: ' + message + '</li>');
+            if (messageFlag == 'turn on'){
+                $('#switchButton').text('turn off');
+                $scope.lightFlag = true;
+                $scope.lightOnCount++;
+            }
+            else{
+                $('#switchButton').text('turn on');
+                $scope.lightFlag = false;
+                $scope.lightOffCount++;
+            }
+            console.log("Works");
+        }
+
 	}
 ]);
