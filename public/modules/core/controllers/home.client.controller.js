eb098674d39ea02511f66445dd2bd34a00c8659f
Kevin Ruiz <kevin.ruiz@coderoad.com>
2015-05-12 22:51:17 -0600

    "Adding Panels to all Rooms"

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,8 +5,13 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
-        $scope.lightStatus1 = false ;
-        $scope.lightStatus2 = false ;
+        $scope.lightStatus1 = true;
+        $scope.lightStatus2 = true;
+        $scope.panelRoom1 = true;
+        $scope.panelRoom2 = false;
+        $scope.panelRoom3 = false;
+        $scope.panelLvRoom = false;
+        $scope.panelKitchen = false;
 
         $scope.tabs = [
             { paneId: 'tab01', title: 'Room 1', content: 'Tab Number 1 Content', active: true, disabled: false },
@@ -16,13 +21,31 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
             { paneId: 'tab05', title: 'Kitchen', content: 'Tab Number 5 Content', active: false, disabled: false }
         ];
 
-        $scope.changeDetect1 = function(id){
-          console.log('Change Detected');
-            //console.log('id: '+id);
-            if (id){
-                console.log('On');
+        var setPanelFlags = function(room1,room2,room3,lvroom,kitchen){
+            $scope.panelRoom1 = room1;
+            $scope.panelRoom2 = room2;
+            $scope.panelRoom3 = room3;
+            $scope.panelLvRoom = lvroom;
+            $scope.panelKitchen = kitchen;
+        };
+
+        $scope.panelView = function(panel){
+          console.log(panel);
+            if(panel=='Room 1'){
+                console.log('1');
+                setPanelFlags(true,false,false,false,false);
+            }else if(panel=='Room 2'){
+                console.log('2');
+                setPanelFlags(false,true,false,false,false);
+            }else if(panel =='Room 3'){
+                console.log('3');
+                setPanelFlags(false,false,true,false,false);
+            }else if(panel == 'Living Room'){
+                console.log('4');
+                setPanelFlags(false,false,false,true,false);
             }else{
-                console.log('Off');
+                console.log('5');
+                setPanelFlags(false,false,false,false,true);
             }
         };
 
@@ -83,7 +106,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
         };
 
         $scope.lightButton = function(status,option){
-            var messageFlag = status? 'turn on':'turn off';
+            var messageFlag = !status? 'turn on':'turn off';
             console.log(option);
             var message = messageFlag+' '+option;
             console.log('message: '+message);
